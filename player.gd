7ecc9645552d3f5810be08f5ac26798b5330a96a
Brandon Olson <olson.l.brandon@gmail.com>
2025-02-27 17:12:43 -0800

    feature: basic playing movement

--- player.gd
+++ player.gd
@@ -7,20 +7,37 @@ const LEFT_BOUND = -72
 const RIGHT_BOUND = 72
 const TOP_BOUND = -120
 
+var tween: Tween
+
+@onready var body: Sprite2D = $Body
+@onready var shadow: Sprite2D = $Shadow
+
+@export var hopTime: float = 0.1
+
 func _process(delta: float) -> void:
-	if (Input.is_action_just_pressed("move_up")):
-		print("pressed move up")
+	if (Input.is_action_pressed("move_up")):
 		if (position.y > TOP_BOUND):
-			position -= Vector2(0, CELL_SIZE)
-	elif (Input.is_action_just_pressed("move_down")):
-		print("pressed move down")
+			move(Vector2(0, -CELL_SIZE))
+	elif (Input.is_action_pressed("move_down")):
 		if (position.y < BOTTOM_BOUND):
-			position += Vector2(0, CELL_SIZE)
-	elif (Input.is_action_just_pressed("move_left")):
-		print("pressed move left")
+			move(Vector2(0, CELL_SIZE))
+	elif (Input.is_action_pressed("move_left")):
 		if (position.x > LEFT_BOUND):
-			position -= Vector2(CELL_SIZE, 0)
-	elif (Input.is_action_just_pressed("move_right")):
-		print("pressed move right")
+			move(Vector2(-CELL_SIZE, 0))
+	elif (Input.is_action_pressed("move_right")):
 		if (position.x < RIGHT_BOUND):
-			position += Vector2(CELL_SIZE, 0)
+			move(Vector2(CELL_SIZE, 0))
+
+func move(direction: Vector2):
+	if (tween and tween.is_running()):
+		return
+	print("animate")
+	tween = create_tween()
+	tween.tween_property(self, "position", position + direction, hopTime)
+	tween.parallel().set_ease(Tween.EASE_OUT).tween_property(body, "position", Vector2(0, -14), hopTime / 2)
+	tween.tween_property(shadow, "scale", Vector2(0.025, 0.025), hopTime / 2)
+	tween.chain().set_ease(Tween.EASE_IN).tween_property(body, "position", Vector2(0, -7), hopTime / 2)
+	tween.tween_property(shadow, "scale", Vector2(0.05, 0.05), hopTime / 2)
+	
+	
+	
