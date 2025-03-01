0966a08249cf1b4776b25bebeba8ccc3c98aa3c9
Brandon Olson <olson.l.brandon@gmail.com>
2025-02-28 21:25:26 -0800

    refactor: improve character movement

--- player.gd
+++ player.gd
@@ -7,15 +7,23 @@ const LEFT_BOUND = -72
 const RIGHT_BOUND = 72
 const TOP_BOUND = -120
 
-var tween: Tween
-var startposition: Vector2
+var start_position: Vector2
+
+@export var up_image: int = 1
+@export var down_image: int = 2
+@export var left_image: int = 0
+@export var right_image: int = 3
+
 @onready var body: Sprite2D = $Body
 @onready var shadow: Sprite2D = $Shadow
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-@export var hopTime: float = 0.1
+const HOP = "hop"
+var is_hopping: bool = false
+var hop_tween: Tween = null
 
 func _ready() -> void:
-	startposition = position
+	start_position = global_position
 
 func _process(delta: float) -> void:
 	if (Input.is_action_pressed("move_up")):
@@ -32,17 +40,30 @@ func _process(delta: float) -> void:
 			move(Vector2(CELL_SIZE, 0))
 
 func move(direction: Vector2):
-	if (tween and tween.is_running()):
+	if (is_hopping):
 		return
-	tween = create_tween()
-	tween.tween_property(self, "position", position + direction, hopTime)
-	tween.parallel().set_ease(Tween.EASE_OUT).tween_property(body, "position", Vector2(0, -14), hopTime / 2)
-	tween.tween_property(shadow, "scale", Vector2(0.025, 0.025), hopTime / 2)
-	tween.chain().set_ease(Tween.EASE_IN).tween_property(body, "position", Vector2(0, -7), hopTime / 2)
-	tween.tween_property(shadow, "scale", Vector2(0.05, 0.05), hopTime / 2)
-	
+	if (direction.x < 0):
+		body.frame = left_image
+	elif direction.x > 0:
+		body.frame = right_image
+	elif direction.y > 0:
+		body.frame = up_image
+	elif direction.y < 0:
+		body.frame = down_image
+	is_hopping = true
+	animation_player.play(HOP)
+	var hop_time = animation_player.get_animation(HOP).length
+	var target_global_position = global_position + direction 
+	hop_tween = create_tween()
+	hop_tween.tween_property(self, "global_position", target_global_position, hop_time)
+	await animation_player.animation_finished
+	is_hopping = false 
 
 func kill():
-	position = startposition
-	tween.kill()
+	if (hop_tween and hop_tween.is_running()):
+		hop_tween.kill()
+		hop_tween = null
+	is_hopping = false
+	animation_player.play("reset")
+	global_position = start_position
 	
