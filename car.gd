5befa5fc889dd2333f1dc3ed395eb0d36ff9bef2
Brandon Olson <olson.l.brandon@gmail.com>
2025-04-10 19:29:03 -0700

    feature: standardize road and car size to one tile wide

--- car.gd
+++ car.gd
@@ -1,17 +1,24 @@
 extends Area2D
+
 @onready var sprite: Sprite2D = $Sprite
 
-const taxi_region = Rect2(240,232,32,24)
-const redcar_region = Rect2(240,264,32,24)
-const convert_region = Rect2(336,272,32,16)
-const car_regions: Array[Rect2] = [taxi_region,redcar_region,convert_region]
+const GREEN_CAR = preload("res://car_textures/green_car.tres")
+const RED_CAR = preload("res://car_textures/red_car.tres")
+const YELLOW_CAR = preload("res://car_textures/yellow_car.tres")
+
+const car_regions: Array[Texture] = [
+	GREEN_CAR, 
+	RED_CAR, 
+	YELLOW_CAR
+]
+
 @export var speed = 16:
 	set(value):
 		speed = value
 		sprite.flip_h = speed < 0
 
 func _ready() -> void:
-	sprite.region_rect = car_regions.pick_random()
+	sprite.texture = car_regions.pick_random()
 
 func _process(delta: float) -> void:
 	position = position + Vector2(speed * delta,0)
